b43399e4c72d6c47b88ac359c22ec7b21813ae30
Gene Cook <genec@genes-cube>
2020-02-26 12:14:23 -0600

    added all (tbd) supported CSRs

--- include/riscv_core_state.h
+++ include/riscv_core_state.h
@@ -18,16 +18,31 @@ public:
   RiscvState(RiscvState *rhs,bool _show_updates = false) : State(rhs) {
     show_updates = _show_updates;
     
-    SetMEPC(rhs->MEPC());
-    SetMTVEC(rhs->MTVEC());
+    SetUSTATUS(rhs->USTATUS());
+    SetFFLAGS(rhs->FFLAGS());
+    SetFRM(rhs->FRM());
+    SetFCSR(rhs->FCSR());
     SetSATP(rhs->SATP());
-    SetPMPADDR0(rhs->PMPADDR0());
-    SetPMPCFG0(rhs->PMPCFG0());
+    SetMSTATUS(rhs->MSTATUS());  
     SetMEDELEG(rhs->MEDELEG());
     SetMIDELEG(rhs->MIDELEG());
     SetMIE(rhs->MIE());
-    SetMSTATUS(rhs->MSTATUS());
-    
+    SetMTVEC(rhs->MTVEC());
+    SetMCOUNTEREN(rhs->MCOUNTEREN());		
+    SetMSTATUSH(rhs->MSTATUSH());
+    SetMSCRATCH(rhs->MSCRATCH());
+    SetMEPC(rhs->MEPC());
+    SetMCAUSE(rhs->MCAUSE());
+    SetMTVAL(rhs->MTVAL());
+    SetMIP(rhs->MIP());
+    SetMTINST(rhs->MTINST());
+    SetPMPCFG0(rhs->PMPCFG0());
+    SetPMPADDR0(rhs->PMPADDR0());
+    SetMCYCLE(rhs->MCYCLE());
+    SetMTINSTRET(rhs->MTINSTRET());
+    SetMCYCLEH(rhs->MCYCLEH());
+    SetMTINSTRETH(rhs->MTINSTRETH());
+
     for (auto i = 0; i < 32; i++) {
        X[i].Value(rhs->X[i].Value());
     }
@@ -36,15 +51,30 @@ public:
   void Update(RiscvState *rhs) {
     State::Update((State *) rhs,show_updates);
     
-    if (MEPC() != rhs->MEPC()) { SetMEPC(rhs->MEPC());   }
-    if (MTVEC() != rhs->MTVEC()) { SetMTVEC(rhs->MTVEC()); }
-    if (SATP() != rhs->SATP()) { SetSATP(rhs->SATP()); }
-    if (PMPADDR0() != rhs->PMPADDR0()) { SetPMPADDR0(rhs->PMPADDR0()); }
-    if (PMPCFG0() != rhs->PMPCFG0()) { SetPMPCFG0(rhs->PMPCFG0()); }
-    if (MEDELEG() != rhs->MEDELEG()) { SetMEDELEG(rhs->MEDELEG()); }
-    if (MIDELEG() != rhs->MIDELEG()) { SetMIDELEG(rhs->MIDELEG()); }
-    if (MIE() != rhs->MIE())   { SetMIE(rhs->MIE()); }
-    if (MSTATUS() != rhs->MSTATUS())   { SetMSTATUS(rhs->MSTATUS()); }
+    if (USTATUS() != rhs->USTATUS()) SetUSTATUS(rhs->USTATUS());
+    if (FFLAGS() != rhs->FFLAGS()) SetFFLAGS(rhs->FFLAGS());
+    if (FRM() != rhs->FRM()) SetFRM(rhs->FRM());
+    if (FCSR() != rhs->FCSR()) SetFCSR(rhs->FCSR());
+    if (SATP() != rhs->SATP()) SetSATP(rhs->SATP());
+    if (MSTATUS() != rhs->MSTATUS()) SetMSTATUS(rhs->MSTATUS());  
+    if (MEDELEG() != rhs->MEDELEG()) SetMEDELEG(rhs->MEDELEG());
+    if (MIDELEG() != rhs->MIDELEG()) SetMIDELEG(rhs->MIDELEG());
+    if (MIE() != rhs->MIE()) SetMIE(rhs->MIE());
+    if (MTVEC() != rhs->MTVEC()) SetMTVEC(rhs->MTVEC());
+    if (MCOUNTEREN() != rhs->MCOUNTEREN()) SetMCOUNTEREN(rhs->MCOUNTEREN());
+    if (MSTATUSH() != rhs->MSTATUSH()) SetMSTATUSH(rhs->MSTATUSH());
+    if (MSCRATCH() != rhs->MSCRATCH()) SetMSCRATCH(rhs->MSCRATCH());
+    if (MEPC() != rhs->MEPC()) SetMEPC(rhs->MEPC());
+    if (MCAUSE() != rhs->MCAUSE()) SetMCAUSE(rhs->MCAUSE());
+    if (MTVAL() != rhs->MTVAL()) SetMTVAL(rhs->MTVAL());
+    if (MIP() != rhs->MIP()) SetMIP(rhs->MIP());	
+    if (MTINST() != rhs->MTINST()) SetMTINST(rhs->MTINST());
+    if (PMPCFG0() != rhs->PMPCFG0()) SetPMPCFG0(rhs->PMPCFG0());
+    if (PMPADDR0() != rhs->PMPADDR0()) SetPMPADDR0(rhs->PMPADDR0());
+    if (MCYCLE() != rhs->MCYCLE()) SetMCYCLE(rhs->MCYCLE());
+    if (MTINSTRET() != rhs->MTINSTRET()) SetMTINSTRET(rhs->MTINSTRET());
+    if (MCYCLEH() != rhs->MCYCLEH()) SetMCYCLEH(rhs->MCYCLEH());
+    if (MTINSTRETH() != rhs->MTINSTRETH()) SetMTINSTRETH(rhs->MTINSTRETH());
 
     for (auto i = 0; i < 32; i++) {
       if (X[i].Value() != rhs->X[i].Value()) {
@@ -111,28 +141,44 @@ public:
   
   std::string CSR_NAME(int csr);
 
-  unsigned long long MEPC() { return _MEPC.Value(); };
-  void SetMEPC(unsigned long long rval) {
-    _MEPC.Value(rval);
-    if (show_updates) printf("  # mepc:\t0x%llx\n",_MEPC.Value());
+  // misa - 32 bits
+  //     31:30 - MXL - 01 (XLEN is 32 bits)
+  //     bit  0 - A atomic extension
+  //          3 - D double-precision floating pt
+  //          5 - F single-precision     "
+  //	    8 - I RV32I base ISA
+  //	   12 - M Integer multiply/divide extension
+  //	   20 - U user mode
+
+  static const unsigned int MISA      = 0x40101100; // 32 bits, user mode supported, rv32i + rv32m
+  static const unsigned int MVENDORID = 0;          // non-commercial so no vendor ID,
+  static const unsigned int MARCHID   = 0;          //       "              machine arch. ID,
+  static const unsigned int MIMPID    = 0;          //                        machine implementation
+
+  unsigned long long USTATUS() { return _USTATUS.Value(); };
+  void SetUSTATUS(unsigned long long rval) {
+    _USTATUS.Value(rval);
   };
-  unsigned long long MTVEC() { return _MTVEC.Value(); };
-  void SetMTVEC(unsigned long long rval) {
-    _MTVEC.Value(rval);
-    if (show_updates) printf("  # mtvec:\t0x%llx\n",_MTVEC.Value());
+  unsigned long long FFLAGS() { return _FFLAGS.Value(); };
+  void SetFFLAGS(unsigned long long rval) {
+    _FFLAGS.Value(rval);
+  };
+  unsigned long long FRM() { return _FRM.Value(); };
+  void SetFRM(unsigned long long rval) {
+    _FRM.Value(rval);
+  };
+  unsigned long long FCSR() { return _FCSR.Value(); };
+  void SetFCSR(unsigned long long rval) {
+    _FCSR.Value(rval);
   };
   unsigned long long SATP() { return _SATP.Value(); };
   void SetSATP(unsigned long long rval) {
     _SATP.Value(rval); 
     if (show_updates) printf("  # satp:\t0x%llx\n",_SATP.Value());
   };
-  unsigned long long PMPADDR0() { return _PMPADDR0.Value(); };
-  void SetPMPADDR0(unsigned long long rval) {
-    _PMPADDR0.Value(rval);
-  };
-  unsigned long long PMPCFG0() { return _PMPCFG0.Value(); };
-  void SetPMPCFG0(unsigned long long rval) {
-    _PMPCFG0.Value(rval);
+  unsigned long long MSTATUS() { return _MSTATUS.Value(); };
+  void SetMSTATUS(unsigned long long rval) {
+    _MSTATUS.Value(rval);
   };
   unsigned long long MEDELEG() { return _MEDELEG.Value(); };
   void SetMEDELEG(unsigned long long rval) {
@@ -146,9 +192,67 @@ public:
   void SetMIE(unsigned long long rval) {
     _MIE.Value(rval);
   };
-  unsigned long long MSTATUS() { return _MSTATUS.Value(); };
-  void SetMSTATUS(unsigned long long rval) {
-    _MSTATUS.Value(rval);
+  unsigned long long MTVEC() { return _MTVEC.Value(); };
+  void SetMTVEC(unsigned long long rval) {
+    _MTVEC.Value(rval);
+    if (show_updates) printf("  # mtvec:\t0x%llx\n",_MTVEC.Value());
+  };
+  unsigned long long MCOUNTEREN() { return _MCOUNTEREN.Value(); };
+  void SetMCOUNTEREN(unsigned long long rval) {
+    _MCOUNTEREN.Value(rval);
+  };
+  unsigned long long MSTATUSH() { return _MSTATUSH.Value(); };
+  void SetMSTATUSH(unsigned long long rval) {
+    _MSTATUSH.Value(rval);
+  };
+  unsigned long long MSCRATCH() { return _MSCRATCH.Value(); };
+  void SetMSCRATCH(unsigned long long rval) {
+    _MSCRATCH.Value(rval);
+  };
+  unsigned long long MEPC() { return _MEPC.Value(); };
+  void SetMEPC(unsigned long long rval) {
+    _MEPC.Value(rval);
+    if (show_updates) printf("  # mepc:\t0x%llx\n",_MEPC.Value());
+  };
+  unsigned long long MCAUSE() { return _MCAUSE.Value(); };
+  void SetMCAUSE(unsigned long long rval) {
+    _MCAUSE.Value(rval);
+  };
+  unsigned long long MTVAL() { return _MTVAL.Value(); };
+  void SetMTVAL(unsigned long long rval) {
+    _MTVAL.Value(rval);
+  };
+  unsigned long long MIP() { return _MIP.Value(); };
+  void SetMIP(unsigned long long rval) {
+    _MIP.Value(rval);
+  };
+  unsigned long long MTINST() { return _MTINST.Value(); };
+  void SetMTINST(unsigned long long rval) {
+    _MTINST.Value(rval);
+  };  
+  unsigned long long PMPCFG0() { return _PMPCFG0.Value(); };
+  void SetPMPCFG0(unsigned long long rval) {
+    _PMPCFG0.Value(rval);
+  };
+  unsigned long long PMPADDR0() { return _PMPADDR0.Value(); };
+  void SetPMPADDR0(unsigned long long rval) {
+    _PMPADDR0.Value(rval);
+  };
+  unsigned long long MCYCLE() { return _MCYCLE.Value(); };
+  void SetMCYCLE(unsigned long long rval) {
+    _MCYCLE.Value(rval);
+  };
+  unsigned long long MTINSTRET() { return _MTINSTRET.Value(); };
+  void SetMTINSTRET(unsigned long long rval) {
+    _MTINSTRET.Value(rval);
+  };
+  unsigned long long MCYCLEH() { return _MCYCLEH.Value(); };
+  void SetMCYCLEH(unsigned long long rval) {
+    _MCYCLEH.Value(rval);
+  };
+  unsigned long long MTINSTRETH() { return _MTINSTRETH.Value(); };
+  void SetMTINSTRETH(unsigned long long rval) {
+    _MTINSTRETH.Value(rval);
   };
 
   // alias riscv gp register indices to common riscv assembly language names...
@@ -186,7 +290,27 @@ private:
   ControlRegister _MIDELEG;
   ControlRegister _MIE;
   ControlRegister _MSTATUS;
+  ControlRegister _MSTATUSH;
   
+  ControlRegister _MCOUNTEREN;    // machine counter enable
+  ControlRegister _MCOUNTINHIBIT; //    "      "     inhibit
+  
+  ControlRegister _MSCRATCH;
+  ControlRegister _MCAUSE;
+  ControlRegister _MTVAL;
+  ControlRegister _MIP;
+  ControlRegister _MTINST;
+  
+  ControlRegister _MCYCLE;     // use master clock?
+  ControlRegister _MCYCLEH;    //      "
+  ControlRegister _MTINSTRET;  // use # of instrs simulated?
+  ControlRegister _MTINSTRETH; //       "
+
+  ControlRegister _USTATUS;    // user status register
+  ControlRegister _FFLAGS;     // floating pt flags
+  ControlRegister _FRM;        //      "      rounding mode
+  ControlRegister _FCSR;       //      "      control/status
+
   bool show_updates; // show register values
   std::vector<std::string> reg_updates;
 };
--- src/riscv_core_state.C
+++ src/riscv_core_state.C
@@ -87,23 +87,55 @@ unsigned long long RiscvState::CSR(unsigned int csr,unsigned int privilege_level
   unsigned long long rval = 0;
   
   switch(csr) {
-    case 0xc00: rval = Clock() & 0xffffffff; break;
-    case 0xc01: rval = TimerValue() & 0xffffffff; break;
-    case 0xc02: rval = InstructionCount() & 0xffffffff; break;
-    case 0xc80: rval = Clock() >> 32; break;
-    case 0xc81: rval = TimerValue() >> 32; break;
-    case 0xc82: rval = InstructionCount() & 0xffffffff; break;
-    case 0x341: rval = MEPC(); break;
-    case 0xf14: rval = GetID(); break;
-    case 0x305: rval = MTVEC(); break;
-    case 0x180: rval = SATP();  break;
-    case 0x3b0: rval = PMPADDR0(); break;
-    case 0x3a0: rval = PMPCFG0(); break;
+    case 0x000: rval = USTATUS(); break;
+
+    case 0x001: rval = FFLAGS(); break;
+    case 0x002: rval = FRM(); break;
+    case 0x003: rval = FCSR(); break;
+
+    case 0x180: rval = SATP(); break;
+
+    case 0x300: rval = MSTATUS(); break;
+    case 0x301: rval = MISA; break;
     case 0x302: rval = MEDELEG(); break;
     case 0x303: rval = MIDELEG(); break;
     case 0x304: rval = MIE(); break;
-    case 0x300: rval = MSTATUS(); break;
+    case 0x305: rval = MTVEC(); break;
+    case 0x306: rval = MCOUNTEREN(); break;
       
+    case 0x310: rval = MSTATUSH(); break;
+      
+    case 0x340: rval = MSCRATCH(); break;
+    case 0x341: rval = MEPC(); break;
+    case 0x342: rval = MCAUSE(); break;
+    case 0x343: rval = MTVAL(); break;
+    case 0x344: rval = MIP(); break;
+      
+    case 0x34A: rval = MTINST(); break;
+
+    case 0x3a0: rval = PMPCFG0(); break;
+      
+    case 0x3b0: rval = PMPADDR0(); break;
+
+    case 0xb00: rval = MCYCLE(); break;
+    case 0xb02: rval = MTINSTRET(); break;
+
+    case 0xb80: rval = MCYCLEH(); break;
+    case 0xb82: rval = MTINSTRETH(); break;
+
+    case 0xc00: rval = Clock() & 0xffffffff; break;
+    case 0xc01: rval = TimerValue() & 0xffffffff; break;
+    case 0xc02: rval = InstructionCount() & 0xffffffff; break;
+      
+    case 0xc80: rval = Clock() >> 32; break;
+    case 0xc81: rval = TimerValue() >> 32; break;
+    case 0xc82: rval = InstructionCount() & 0xffffffff; break;
+      
+    case 0xf11: rval = MVENDORID; break;
+    case 0xf12: rval = MARCHID; break;
+    case 0xf13: rval = MIMPID; break;
+    case 0xf14: rval = GetID(); break;
+  
     default: throw ILLEGAL_INSTRUCTION;
              break;
   }
@@ -116,24 +148,42 @@ void RiscvState::SetCSR(unsigned int csr,unsigned int privilege_level, unsigned 
   ValidateCSRAccess(csr,privilege_level,true);
   
   switch(csr) {
-    case 0x341: SetMEPC(rval);
-                break;
-    case 0x305: SetMTVEC(rval);
-                break;
-    case 0x180: SetSATP(rval);
-                break;
-    case 0x3b0: SetPMPADDR0(rval);
-                break;
-    case 0x3a0: SetPMPCFG0(rval);
-                break;
-    case 0x302: SetMEDELEG(rval);
-		break;
-    case 0x303: SetMIDELEG(rval);
-		break;
-    case 0x304: SetMIE(rval);
-                break;
-    case 0x300: SetMSTATUS(rval);
-                break;
+    case 0x000: SetUSTATUS(rval); break;
+
+    case 0x001: SetFFLAGS(rval); break;
+    case 0x002: SetFRM(rval); break;
+    case 0x003: SetFCSR(rval); break;
+      
+    case 0x180: SetSATP(rval); break;
+		
+    case 0x300: SetMSTATUS(rval); break;
+      
+    case 0x302: SetMEDELEG(rval); break;
+    case 0x303: SetMIDELEG(rval); break;
+    case 0x304: SetMIE(rval); break;
+    case 0x305: SetMTVEC(rval); break;
+    case 0x306: SetMCOUNTEREN(rval); break;
+		
+    case 0x310: SetMSTATUSH(rval); break;
+
+    case 0x340: SetMSCRATCH(rval); break;
+    case 0x341: SetMEPC(rval); break;
+    case 0x342: SetMCAUSE(rval); break;
+    case 0x343: SetMTVAL(rval); break;
+    case 0x344: SetMIP(rval); break;
+		
+    case 0x34A: SetMTINST(rval); break;
+
+    case 0x3a0: SetPMPCFG0(rval); break;
+		
+    case 0x3b0: SetPMPADDR0(rval); break;
+		
+    case 0xb00: SetMCYCLE(rval); break;
+    case 0xb02: SetMTINSTRET(rval); break;
+
+    case 0xb80: SetMCYCLEH(rval); break;
+    case 0xb82: SetMTINSTRETH(rval); break;
+
     default:    throw ILLEGAL_INSTRUCTION;
                 break;
   }
@@ -144,24 +194,53 @@ void RiscvState::SetCSR(unsigned int csr,unsigned int privilege_level, unsigned 
 std::string RiscvState::CSR_NAME(int csr) {
     std::string rname;
     char tbuf[16];
-    switch(csr) {
-      case 0xc00: rname = "rdcycle";    break;
-      case 0xc01: rname = "rdtime";     break;
-      case 0xc02: rname = "rdinstret";  break;
-      case 0xc80: rname = "rdcycleh";   break;
-      case 0xc81: rname = "rdtimeh";    break;
-      case 0xc82: rname = "rdinstreth"; break;
-      case 0xf14: rname = "mhartid";    break;
-      case 0x305: rname = "mtvec";      break;
+    switch(csr) {	
+      case 0x000: rname = "ustatus";    break;
+
+      case 0x001: rname = "fflags";     break;
+      case 0x002: rname = "frm";        break;
+      case 0x003: rname = "fcsr";       break;
+      
       case 0x180: rname = "satp";       break;
-      case 0x3b0: rname = "pmpaddr0";   break;
-      case 0x3a0: rname = "pmpcfg0";    break;
+	
+      case 0x300: rname = "mstatus";    break;
+
+      case 0x301: rname = "misa";       break;
       case 0x302: rname = "medeleg";    break;
       case 0x303: rname = "mideleg";    break;
       case 0x304: rname = "mie";        break;
-      case 0x300: rname = "mstatus";    break;
-      case 0x341: rname = "mepc";       break;
+      case 0x305: rname = "mtvec";      break;
+      case 0x306: rname = "mcounteren"; break;
 
+      case 0x310: rname = "mstatush";   break;
+
+      case 0x340: rname = "mscratch";   break;
+      case 0x341: rname = "mepc";       break;
+      case 0x342: rname = "mcause";     break;
+      case 0x343: rname = "mtval";      break;
+      case 0x344: rname = "mip";        break;
+		
+      case 0x34A: rname = "mtinst";     break;
+
+      case 0x3a0: rname = "pmpcfg0";    break;
+      case 0x3b0: rname = "pmpaddr0";   break;
+	
+      case 0xb00: rname = "mycycle";    break;
+      case 0xb02: rname = "mtinstret";  break;
+
+      case 0xb80: rname = "mycycleh";   break;
+      case 0xb82: rname = "mtinstreth"; break;
+      
+      case 0xc00: rname = "rdcycle";    break;
+      case 0xc01: rname = "rdtime";     break;
+      case 0xc02: rname = "rdinstret";  break;
+	
+      case 0xc80: rname = "rdcycleh";   break;
+      case 0xc81: rname = "rdtimeh";    break;
+      case 0xc82: rname = "rdinstreth"; break;
+	
+      case 0xf14: rname = "mhartid";    break;
+	
       default: //throw EXCEPTION;
                sprintf(tbuf,"<0x%03x>",csr);
                rname = tbuf;
