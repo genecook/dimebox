0d4973cfa04192941b5e083fe9e675055484a527
Gene Cook <genec@genes-cube>
2020-04-07 09:27:21 -0500

    add code to improve disassembly for instrs with immnediates

--- utils/make_rv32i_iclasses.pl
+++ utils/make_rv32i_iclasses.pl
@@ -12,19 +12,12 @@ my @J = qw(JAL);
 my @I = qw(ADDI SLTI XORI ORI ANDI);
 
 my @I_CSRR = qw(CSRRW CSRRC);
-
 my @I_CSRS = qw(CSRRS);
-
 my @I_CSRI = qw(CSRRWI CSRRSI CSRRCI);
-
 my @I_UNS = qw(SLTIU);
-
 my @I_SHIFT = qw(SLLI SRLI SRAI);
-
 my @I_JALR = qw(JALR);
-    
 my @I_LOAD = qw(LB LH LW LBU LHU);
-
 my @I_TRAP = qw(URET SRET HRET MRET WFI SFENCE);
 
 my @S = qw(SB SH SW);
@@ -46,6 +39,41 @@ my @CS   = qw( C.SW C.AND C.OR C.XOR C.SUB );
 my @CB   = qw( C.BEQZ C.BNEZ C.SRLI C.SRAI C.ANDI );
 my @CJ   = qw( C.J C.JAL );
 
+# instruction specific immediates
+# (added to Decode method called prior to Disassembly):
+
+my %decodes = (
+    'ADDI'   => 'SIGN_EXTEND_IMM(12)',
+    'SLTI'   => 'SIGN_EXTEND_IMM(12)',
+    'XORI'   => 'SIGN_EXTEND_IMM(12)',
+    'ORI'    => 'SIGN_EXTEND_IMM(12)',
+    'ANDI'   => 'SIGN_EXTEND_IMM(12)',
+    'SLTIU'  => 'UNSIGNED_SIGN_EXTEND_IMM(12)',
+    'JAL'    => 'SIGN_EXTEND_IMM(20)',
+    'JALR'   => 'SIGN_EXTEND_IMM(12)',
+    'BEQ'    => 'SIGN_EXTEND_IMM(13)',
+    'BNE'    => 'SIGN_EXTEND_IMM(13)',
+    'BLT'    => 'SIGN_EXTEND_IMM(13)',
+    'BLTU'   => 'SIGN_EXTEND_IMM(13)',
+    'BGE'    => 'SIGN_EXTEND_IMM(13)',
+    'BGEU'   => 'SIGN_EXTEND_IMM(13)',
+    'LB'     => 'SIGN_EXTEND_IMM(12)',
+    'LH'     => 'SIGN_EXTEND_IMM(12)',
+    'LW'     => 'SIGN_EXTEND_IMM(12)',
+    'LBU'    => 'SIGN_EXTEND_IMM(12)',
+    'LHU'    => 'SIGN_EXTEND_IMM(12)',
+    'SB'     => 'SIGN_EXTEND_IMM(12)',
+    'SH'     => 'SIGN_EXTEND_IMM(12)',
+    'SW'     => 'SIGN_EXTEND_IMM(12)',
+    'C.J'    => 'SIGN_EXTEND_IMM(11)',
+    'C.JAL'  => 'SIGN_EXTEND_IMM(11)',
+    'C.BEQZ' => 'SIGN_EXTEND_IMM(8)',
+    'C.BNEZ' => 'SIGN_EXTEND_IMM(8)',
+    'C.SRAI' => 'IMM_FOR_SHIFT()',
+    'C.ANDI' => 'SIGN_EXTEND_IMM(6)'
+);
+
+
 my $instr_template = '
 class $INSTR$ : public $TYPE$typeInstruction {
   public:
@@ -53,6 +81,7 @@ class $INSTR$ : public $TYPE$typeInstruction {
     : $TYPE$typeInstruction(_state,_memory,_encoding) { $INIT$; };
     std::string InstrName() { return std::string("$instr$"); };
     void Step(); 
+    void Decode() { $TYPE$typeInstruction::Decode(); $DECODE$; };
 };
 ';
 
@@ -60,38 +89,40 @@ print "#ifndef __RV32IMC_ICLASSES__\n\n";
 
 print "// !!! AUTO-GENERATED CODE - SEE utils/make_rv32i_iclasses.pl !!!\n";
 
-foreach $instr (@U)       { &class_decl($instr,'U','');                             }
-foreach $instr (@U_AUIPC) { &class_decl($instr,'U','uaui=true');                    }
-foreach $instr (@J)       { &class_decl($instr,'J','jal=true');                     }
-foreach $instr (@I)       { &class_decl($instr,'I','');                             }
-foreach $instr (@I_JALR)  { &class_decl($instr,'I','jal=true');                     }
-foreach $instr (@I_LOAD)  { &class_decl($instr,'I','load_store=true');              }
-foreach $instr (@I_UNS)   { &class_decl($instr,'I','unsigned_sign_extension=true'); }
-foreach $instr (@I_SHIFT) { &class_decl($instr,'I','shift=true');                   }
-foreach $instr (@I_CSRS)  { &class_decl($instr,'I','csrs=true');                    }
-foreach $instr (@I_CSRR)  { &class_decl($instr,'I','csrr=true');                    }
-foreach $instr (@I_CSRI)  { &class_decl($instr,'I','csri=true');                    }
-foreach $instr (@I_TRAP)  { &class_decl($instr,'I','trap=true');                    }
-foreach $instr (@S)       { &class_decl($instr,'S','load_store=true');              }
-foreach $instr (@B)       { &class_decl($instr,'B','');                             }
-foreach $instr (@R)       { &class_decl($instr,'R','');                             }
+foreach $instr (@U)       { &class_decl($instr,'U','',                             $decodes{$instr}); }
+foreach $instr (@U_AUIPC) { &class_decl($instr,'U','uaui=true',                    $decodes{$instr}); }
+foreach $instr (@J)       { &class_decl($instr,'J','jal=true',                     $decodes{$instr}); }
+foreach $instr (@I)       { &class_decl($instr,'I','',                             $decodes{$instr}); }
+foreach $instr (@I_JALR)  { &class_decl($instr,'I','jal=true',                     $decodes{$instr}); }
+foreach $instr (@I_LOAD)  { &class_decl($instr,'I','load_store=true',              $decodes{$instr}); }
+foreach $instr (@I_UNS)   { &class_decl($instr,'I','unsigned_sign_extension=true', $decodes{$instr}); }
+foreach $instr (@I_SHIFT) { &class_decl($instr,'I','shift=true',                   $decodes{$instr}); }
+foreach $instr (@I_CSRS)  { &class_decl($instr,'I','csrs=true',                    $decodes{$instr}); }
+foreach $instr (@I_CSRR)  { &class_decl($instr,'I','csrr=true',                    $decodes{$instr}); }
+foreach $instr (@I_CSRI)  { &class_decl($instr,'I','csri=true',                    $decodes{$instr}); }
+foreach $instr (@I_TRAP)  { &class_decl($instr,'I','trap=true',                    $decodes{$instr}); }
+foreach $instr (@S)       { &class_decl($instr,'S','load_store=true',              $decodes{$instr}); }
+foreach $instr (@B)       { &class_decl($instr,'B','',                             $decodes{$instr}); }
+foreach $instr (@R)       { &class_decl($instr,'R','',                             $decodes{$instr}); }
 
-foreach $instr (@CR)   { &class_decl($instr,'CR', '');                }
-foreach $instr (@CR_J) { &class_decl($instr,'CR', 'jal=true');        }
-foreach $instr (@CI)   { &class_decl($instr,'CI', '');                }
-foreach $instr (@CSS)  { &class_decl($instr,'CSS','');                }
-foreach $instr (@CIW)  { &class_decl($instr,'CIW','');                }
-foreach $instr (@CL)   { &class_decl($instr,'CL', 'load_store=true'); }
-foreach $instr (@CS)   { &class_decl($instr,'CS', 'load_store=true'); }
-foreach $instr (@CB)   { &class_decl($instr,'CB', '');                }
-foreach $instr (@CJ)   { &class_decl($instr,'CJ', 'jal=true');        }
+foreach $instr (@CR)      { &class_decl($instr,'CR', '',                $decodes{$instr}); }
+foreach $instr (@CR_J)    { &class_decl($instr,'CR', 'jal=true',        $decodes{$instr}); }
+foreach $instr (@CI)      { &class_decl($instr,'CI', '',                $decodes{$instr}); }
+foreach $instr (@CSS)     { &class_decl($instr,'CSS','',                $decodes{$instr}); }
+foreach $instr (@CIW)     { &class_decl($instr,'CIW','',                $decodes{$instr}); }
+foreach $instr (@CL)      { &class_decl($instr,'CL', 'load_store=true', $decodes{$instr}); }
+foreach $instr (@CS)      { &class_decl($instr,'CS', 'load_store=true', $decodes{$instr}); }
+foreach $instr (@CB)      { &class_decl($instr,'CB', '',                $decodes{$instr}); }
+foreach $instr (@CJ)      { &class_decl($instr,'CJ', 'jal=true',        $decodes{$instr}); }
 
 print "\n#endif\n";
 print "#define __RV32IMC_ICLASSES__ 1\n";
 
 sub class_decl {
-    my ($instr,$type,$init) = @_;
+    my ($instr,$type,$init,$decode) = @_;
 
+    $decode = '' if not defined($decode);
+    
     my $instr_lc = lc $instr;
     $instr =~ s/\./_/;
     my $itemp = $instr_template;
@@ -99,5 +130,6 @@ sub class_decl {
     $itemp =~ s/\$instr\$/$instr_lc/g;
     $itemp =~ s/\$TYPE\$/$type/g;
     $itemp =~ s/\$INIT\$/$init/g;
+    $itemp =~ s/\$DECODE\$/$decode/g;
     print $itemp;
 }
